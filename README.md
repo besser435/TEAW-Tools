8d8341dcf6d9383c9a7d42f3b86dcb7fcee182b9
besser <68145204+besser435@users.noreply.github.com>
2025-06-30 09:50:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TEAW-Tools
+A plugin to help manage miscellaneous TEAW functions.
---
+++ src/main/java/org/besser/teto/Teto.java
@@ -0,0 +1,17 @@
+package org.besser.teto;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Teto extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
