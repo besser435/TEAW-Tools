6016179db1671cc22f283ce98375756f60371ee1
besser <68145204+besser435@users.noreply.github.com>
2026-01-27 23:11:36 -0700

    Fix unable to change even when unlocked

--- src/main/java/org/besser/teto/Commands/Towny/MapColor/TownColorChangeListener.java
+++ src/main/java/org/besser/teto/Commands/Towny/MapColor/TownColorChangeListener.java
@@ -3,6 +3,7 @@ package org.besser.teto.Commands.Towny.MapColor;
 import com.palmergames.bukkit.towny.TownyAPI;
 import com.palmergames.bukkit.towny.object.Nation;
 import com.palmergames.bukkit.towny.object.Resident;
+import com.palmergames.bukkit.towny.object.metadata.BooleanDataField;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,11 +14,11 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 public class TownColorChangeListener implements Listener {
     @EventHandler(priority = EventPriority.LOW)
     public void onCommandPreprocess(PlayerCommandPreprocessEvent event) {
-        String msg = event.getMessage().toLowerCase();
+        String msg = event.getMessage().toLowerCase().trim();
 
         // This is kind of janky because it's a hard coded string, but whatever, it's the best we can do with Towny's API.
-        boolean isMapcolor = msg.startsWith("/t set mapcolor") || msg.startsWith("/t set mapcolor");
-        if (!isMapcolor) return;
+        boolean isMapColor = msg.startsWith("/t set mapcolor") || msg.startsWith("/town set mapcolor");
+        if (!isMapColor) return;
 
         Player player = event.getPlayer();
         Resident resident = TownyAPI.getInstance().getResident(player);
@@ -28,8 +29,14 @@ public class TownColorChangeListener implements Listener {
         if (nation == null) return;
 
         // Lock check and messages
-        boolean locked = nation.hasMeta("teto_mapcolor_locked");
-        if (!locked) return;
+        boolean isLocked = false;
+        if (nation.hasMeta("teto_mapcolor_locked")) {
+            BooleanDataField lockField = (BooleanDataField) nation.getMetadata("teto_mapcolor_locked");
+            if (lockField != null && lockField.getValue()) {
+                isLocked = true;
+            }
+        }
+        if (!isLocked) return;
 
         event.setCancelled(true);
 
